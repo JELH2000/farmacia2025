1630d02840fa75c0a176bcf65dbc0e7d95636336
JELH2000 <lopezjavierx636@gmail.com>
2025-11-28 01:53:56 -0600

    revision5

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ if (!defined('CONFIG')) {
     require_once './config.php';
 }
 
+
 if (isset($_GET['url'])) if ($_GET['url'] == '' || $_GET['url'] == 'index.php') {
     header('Location: ' . HOMEPAGE);
     exit();
 }
 
 $url = $_GET['url'];
-
+echo $url;
+/*
 $urlExist = in_array($url, ROUTE_DIR_EXIST);
 $estaRestringido = in_array($url, FILE_UNAUTHORIZED);
-
-echo "<script>alert('hola mundo');</script>";
 
 if ($urlExist) {
     if (!is_dir(__DIR__ . '/' . $url)) header('Content-Type:' . EXTENTION_FILE[pathinfo($url, PATHINFO_EXTENSION)]);
@@ -38,3 +38,4 @@ if ($urlExist) {
     header('Location: ' . 'http://' . HOST_ROOT . '/' . 'vistas/error/page404.html');
     exit();
 }
+*/

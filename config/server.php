945a9c1817c50a1fdd05e99e773a747e5d7c1c84
JELH2000 <lopezjavierx636@gmail.com>
2025-11-28 12:38:51 -0600

    revision 36

--- config/server.php
+++ config/server.php
@@ -1,6 +1,6 @@
 <?php
 
-	const DB_SERVER="localhost";
-	const DB_NAME="farmacia2";
-	const DB_USER="root";
-	const DB_PASS='';
+const DB_SERVER = "hopper.proxy.rlwy.net:54056";
+const DB_NAME = "farmacia";
+const DB_USER = "root";
+const DB_PASS = 'QZvNxmNRDUqxWxIWQsljnPWRfcHKvIgZ';
